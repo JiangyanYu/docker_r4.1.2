cdf0dca0dfc396d7e8c609152ee22068ba5d2d90
Jiangyan Yu <56250305+JiangyanYu@users.noreply.github.com>
2022-06-13 14:28:14 +0200

    Update run_dockerr4.1.2v1.sh

--- run_dockerr4.1.2v1.sh
+++ run_dockerr4.1.2v1.sh
@@ -1,1 +1,4 @@
-docker run --rm -p 8787:8787 -e USER=jyu -e PASSWORD=jyu -e ROOT=TRUE -v /home/yu.j/sciebo:/home/jyu/rstudio jiangyanyu/jyu_r4.1.2:1.0
+docker run --rm -p 8787:8787 -e USER=yu.j -e PASSWORD=yu.j -e ROOT=TRUE -v /home/yu.j/sciebo:/home/jyu/rstudio jiangyanyu/jyu_r4.1.2:1.0
+
+## remember to change the permission back if you use a different username
+# sudo chown -R yu.j ./sciebo/
